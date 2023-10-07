399edf9c5447b2340e005e37f3b7e263ea522325
ijosue16 <iradukundajosue16@gmail.com>
2023-10-07 21:03:33 +0200

    added books section in students page

--- src/students interaction/UserNavbar.js
+++ src/students interaction/UserNavbar.js
@@ -1,12 +1,24 @@
-import { AppBar, Avatar, Toolbar,Box, Typography } from "@mui/material";
-import React from "react";
+import { AppBar, Avatar, Toolbar,Box, Typography,Menu, MenuItem  } from "@mui/material";
+import React,{useState} from "react";
 import FlexBetween from "../components/FlexBetween";
 import { AutoStoriesOutlined, NotificationsOutlined, SettingsOutlined } from "@mui/icons-material";
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 import { useNavigate } from "react-router-dom";
 
 
 const UserNavbar = ({studentId,userName}) => {
     const navigate=useNavigate();
+    const [anchorElBook, setAnchorElBook] = useState(null);
+    const openBook = Boolean(anchorElBook);
+
+    const handleClickBook = (event) => {
+        setAnchorElBook(event.currentTarget);
+    };
+
+    
+    const handleCloseBook = () => {
+        setAnchorElBook(null);
+    };
     return (
 
         <>
@@ -30,7 +42,7 @@ const UserNavbar = ({studentId,userName}) => {
                     <FlexBetween p={"0px 10px"} gap="20px" alignItems="center">
                     {/* <Typography sx={{fontSize:"20px",fontWeight:"bold"}} onClick={()=>navigate(`/students/newnotifications/${studentId} `)} >notifications</Typography> */}
                     <NotificationsOutlined  sx={{fontSize:"28px"}} onClick={()=>navigate(`/students/newnotifications/${studentId} `)}/>
-                       
+                    <MenuBookIcon   sx={{fontSize:"28px"}} onClick={handleClickBook} />
                         <Avatar
                                         alt="profile"
                                         // src="https://media.istockphoto.com/id/502581380/photo/portrait-of-an-african-american-man-with-glasses.jpg?s=612x612&w=0&k=20&c=cjS24yXM56lie7N-KsFtDH2CKCaS03OQFxwJgIag0ac="
@@ -43,6 +55,23 @@ const UserNavbar = ({studentId,userName}) => {
                     </FlexBetween>
 
                 </Toolbar>
+                <Menu
+                id="user-menu"
+                anchorEl={anchorElBook}
+                open={openBook}
+                onClose={handleCloseBook}
+                MenuListProps={{
+                    'aria-labelledby': 'gauge-button',
+                }}
+                sx={{
+                    "& .MuiList-root": {
+                        backgroundColor: ""
+                    }
+                }}
+            >
+                <MenuItem> <a className="text-decoration-none text-white " href="https://elearning.reb.rw/course/index.php?categoryid=13" target="_blank" rel="noopener noreferrer">O-level Books</a></MenuItem>
+                <MenuItem> <a className="text-decoration-none text-white " href="https://elearning.reb.rw/course/index.php?categoryid=32" target="_blank" rel="noopener noreferrer">A-level Books</a></MenuItem>
+            </Menu>
             </AppBar>
         </>
     )
